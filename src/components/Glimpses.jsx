de4079321ab484ee60d551afee77a160e36164d2
Harsh-0805 <harshbadala0805@gmail.com>
2023-08-27 18:54:44 +0530

    fix glimpses

--- src/components/Glimpses.jsx
+++ src/components/Glimpses.jsx
@@ -1,64 +1,145 @@
-import { v4 as uuidv4 } from "uuid";
-import Card from "./card";
-import Carroussel from "./carousel";
-// import  Carousel  from "./Carousel";S
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "../index.css";
 
-import { styles } from "../styles";
+const Glimpses = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState(null);
 
-function Glimpses() {
-  let cards = [
-    {
-      key: uuidv4(),
-      content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
-      ),
-    },
-    {
-      key: uuidv4(),
-      content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
-      ),
-    },
-    {
-      key: uuidv4(),
-      content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
-      ),
-    },
-    {
-      key: uuidv4(),
-      content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
-      ),
-    },
-    {
-      key: uuidv4(),
-      content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
-      ),
-    },
+  const images = [
+    "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   ];
 
-  return (
-    <>
-    <div className="items-center text-center">
-          <h2 className={styles.sectionHeadText}>
-            GLIMPSES
-          </h2>
-        </div>
-    <div className="flex justify-center items-center h-full px-20 font-sans">
-      <Carroussel
-        cards={cards}
-        height="500px"
-        width="100%"
-        margin="20px auto"
-        padding="50px 20px"
-        offset={2}
-        showArrows={true}
-      />
-    </div>
-    </>
-  );
-}
+  const slideVariants = {
+    hiddenRight: {
+      x: "100%",
+      opacity: 0,
+    },
+    hiddenLeft: {
+      x: "-100%",
+      opacity: 0,
+    },
+    visible: {
+      x: "0",
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+    exit: {
+      opacity: 0,
+      scale: 0.8,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+  const slidersVariants = {
+    hover: {
+      scale: 1.2,
+      backgroundColor: "#ff00008e",
+    },
+  };
+  const dotsVariants = {
+    initial: {
+      y: 0,
+    },
+    animate: {
+      y: -10,
+      scale: 1.2,
+      transition: { type: "spring", stiffness: 1000, damping: "10" },
+    },
+    hover: {
+      scale: 1.1,
+      transition: { duration: 0.2 },
+    },
+  };
 
+  const handleNext = () => {
+    setDirection("right");
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === images.length ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevious = () => {
+    setDirection("left");
+
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleDotClick = (index) => {
+    setDirection(index > currentIndex ? "right" : "left");
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="carousel">
+      <div className="carousel-images">
+        <AnimatePresence>
+          <motion.img
+            key={currentIndex}
+            src={images[currentIndex]}
+            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
+            animate="visible"
+            exit="exit"
+            variants={slideVariants}
+          />
+        </AnimatePresence>
+        <div className="slide_direction">
+          <motion.div
+            variants={slidersVariants}
+            whileHover="hover"
+            className="left"
+            onClick={handlePrevious}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              viewBox="0 96 960 960"
+              width="20"
+            >
+              <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+            </svg>
+          </motion.div>
+          <motion.div
+            variants={slidersVariants}
+            whileHover="hover"
+            className="right"
+            onClick={handleNext}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              viewBox="0 96 960 960"
+              width="20"
+            >
+              <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+            </svg>
+          </motion.div>
+        </div>
+      </div>
+      <div className="carousel-indicator">
+        {images.map((_, index) => (
+          <motion.div
+            key={index}
+            className={`dot ${currentIndex === index ? "active" : ""}`}
+            onClick={() => handleDotClick(index)}
+            initial="initial"
+            animate={currentIndex === index ? "animate" : ""}
+            whileHover="hover"
+            variants={dotsVariants}
+          ></motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Glimpses;
